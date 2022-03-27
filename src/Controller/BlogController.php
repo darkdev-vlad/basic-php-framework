2710b679cbb9eb5c87b5df97bcfd203cfe49d5f1
Vladislav Shadiuk <vlad.developer9@gmail.com>
2022-03-27 18:13:23 +0200

    Added twig rendering engine

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,11 +5,20 @@ declare(strict_types=1);
 namespace Xvladx\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 class BlogController
 {
     public function entryAction(int $year, int $month, int $day): Response
     {
-        return new Response('Unknown reakajsjk');
+        $loader = new FilesystemLoader('../templates');
+        $twig = new Environment($loader);
+
+        $test = $twig->render('blog/entry.html.twig', [
+            'year' => $year
+        ]);
+
+        return new Response($test);
     }
 }
